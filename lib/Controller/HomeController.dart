6f40be87b734f32408fc39a7564316dbd0065a7e
Divya Shaparia <shaparia95@gmail.com>
2023-10-09 20:12:03 +0530

    Movies Added

--- lib/Controller/HomeController.dart
+++ lib/Controller/HomeController.dart
@@ -30,6 +30,48 @@ class HomeController extends GetxController {
             imageUrl:
                 'https://m.media-amazon.com/images/S/pv-target-images/3028aff09398ecc49b7cbf7ca513a215c2f27239c835cce7a8a8dbb8f6e0372f.jpg',
           ),
+          Movie(
+            name: 'Jawaan',
+            description: '2023 ‧ 2h 45m',
+            imageUrl:
+                'https://upload.wikimedia.org/wikipedia/en/3/39/Jawan_film_poster.jpg',
+          ),
+          Movie(
+            name: 'K.G.F: Chapter 1',
+            description: '2018 ‧ 2h 35m',
+            imageUrl:
+                'https://m.media-amazon.com/images/M/MV5BZDNlNzBjMGUtYTA0Yy00OTI2LWJmZjMtODliYmUyYTI0OGFmXkEyXkFqcGdeQXVyODIwMDI1NjM@._V1_.jpg',
+          ),
+          Movie(
+            name: 'K.G.F: Chapter 2',
+            description: '2022 ‧ Action/Drama ‧ 2h 48m',
+            imageUrl:
+                'https://m.media-amazon.com/images/S/pv-target-images/f5a21f64af7359f7aaa7c29dee8a12a97630e707cdbf71e0ae6b063322fb8575.jpg',
+          ),
+          Movie(
+            name: 'Baahubali: The Beginning',
+            description: '2015 ‧ Action/War ‧ 2h 38m',
+            imageUrl:
+                'https://w0.peakpx.com/wallpaper/383/545/HD-wallpaper-baahubali-baahubali-2-bollywood-love-prabhaas-prabhas-rana-shivudu-telugu-thumbnail.jpg',
+          ),
+          Movie(
+            name: 'Baahubali 2: The Conclusion',
+            description: '2017 ‧ Action/Adventure ‧ 2h 47m',
+            imageUrl:
+                'https://m.media-amazon.com/images/I/71i8a-PnChL._AC_UF1000,1000_QL80_.jpg',
+          ),
+          Movie(
+            name: 'Airlift',
+            description: '2016 ‧ Thriller/Drama ‧ 2h 5m',
+            imageUrl:
+                'https://upload.wikimedia.org/wikipedia/en/3/35/Airlift_poster.jpg',
+          ),
+          Movie(
+            name: 'Jailer',
+            description: '2023 ‧ Action/Crime ‧ 2h 48m',
+            imageUrl:
+                'https://m.media-amazon.com/images/M/MV5BMTJmZmQ2OGUtZTg0NS00ZmYzLWJjOGItYmFkNGYwMDM0NTQxXkEyXkFqcGdeQXVyMTY3ODkyNDkz._V1_.jpg',
+          ),
         ],
       );
       isLoading.value = false;
